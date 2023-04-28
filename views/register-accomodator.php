7069ddd478b21c68a5e6721429fc18a6a771db2a
@githubAdmin123 <certgendict@gmail.com>
2023-04-28 13:27:37 +0800

    oush

--- views/register-accomodator.php
+++ views/register-accomodator.php
@@ -5,46 +5,96 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-md-8">
-				<h1 class="text-center">Accommodator</h1>
-				<form action = "../php/reg-accom-process.php" method = "POST">
-					<div class="form-group">
-						<label for="email">Enter your email address here: </label>
-						<input type="email" class="form-control" name="email" id="email" placeholder="Email">
-					</div>
-					<div class="form-group">
-						<label for="password">Enter your password here: </label>
-						<input type="password" class="form-control" name="password" id="password" placeholder="Password">
-					</div>
-					<div class="form-group">
-						<label for="confirm_password">Confirm Password:</label>
-						<input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="Confirm Password">
-					</div>
-					<div class="form-group">
-						<label for="contact_number">Contact no.</label>
-						<input type="tel" class="form-control" name="contact_number" id="contact_number" placeholder="Enter your contact number">
-					</div>
-                    <div class="form-group">
-						<label for="name">Business Info:</label>
-						<input type="text" class="form-control" name="name" id="name" placeholder="Name">
-					</div>
-					<div class="form-group">
-						<label for="business_name">Business Name:</label>
-						<input type="text" class="form-control" name="business_name" id="business_name" placeholder="Business name">
-					</div>
-					<div class="form-group">
-						<label for="address">Location: </label>
-						<input type="text" class="form-control" name="address" id="address" placeholder="Address">
-					</div>
-					<button type="submit" class="btn btn-primary">Register</button>
-				</form>
-                <a href="#" class="text-info font-weight-bold">Back</a>
-			</div>
-		</div>
-	</div>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ffc5a5;">
+  <a class="navbar-brand" href="#">
+  	<img src="../images/home-logo.jpg" alt="Home Logo" class="img-fluid" style="width:80px; height:80px; margin:70px">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<section style="margin-top: 20px;">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <h1 class="text-center">Accommodator</h1>
+        <form action="../php/reg-accom-process.php" method="POST">
+          <div class="form-group">
+            <label for="email">Enter your email address here: </label>
+            <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <label for="password">Enter your password here: </label>
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <label for="confirm_password">Confirm Password:</label>
+            <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="Confirm Password">
+          </div>
+          <div class="form-group">
+            <label for="contact_number">Contact no.</label>
+            <input type="tel" class="form-control" name="contact_number" id="contact_number" placeholder="Enter your contact number">
+          </div>
+          <div class="form-group">
+            <label for="name">Business Info:</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Name">
+          </div>
+          <div class="form-group">
+            <label for="business_name">Business Name:</label>
+            <input type="text" class="form-control" name="business_name" id="business_name" placeholder="Business name">
+          </div>
+          <div class="form-group">
+            <label for="address">Location: </label>
+            <input type="text" class="form-control" name="address" id="address" placeholder="Address">
+          </div>
+          <button type="submit" class="btn btn-primary">Register</button>
+        </form>
+        <a href="#" class="text-info font-weight-bold">Back</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<footer class="footer fixed-bottom py-3 bg-light">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-auto">
+        <a href="#">
+          <i class="fab fa-facebook fa-sm mx-3"></i>
+        </a>
+        <a href="#">
+          <i class="fab fa-twitter fa-sm mx-3"></i>
+        </a>
+        <a href="#">
+          <i class="fab fa-instagram fa-sm mx-3"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- Bootstrap JS and jQuery -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
