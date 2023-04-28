60a188ad2e227fdb1dfe19bce6be2abc08ad3695
Mr-Hearted <hanzbausa123@gmail.com>
2023-04-28 13:41:17 +0800

    commit

---
+++ views/get-started.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <!-- Slide 1 -->
+<div class="tutorial-slide">
+  <h2>Welcome to our site!</h2>
+  <p>We're excited to have you here. This tutorial will help you get started.</p>
+  <button class="next-slide-btn">Next</button>
+</div>
+
+<!-- Slide 2 -->
+<div class="tutorial-slide">
+  <h2>Getting Started</h2>
+  <p>Here are a few things you can do to get started:</p>
+  <ul>
+    <li>Update your profile information</li>
+    <li>Explore our site and find interesting content</li>
+    <li>Connect with other users and start building your network</li>
+  </ul>
+  <form method="post" action="homepage.php">
+  <input type="submit" value="Go to Other File">
+</form>
+</div>
+
+</body>
+</html>
