aad5ff1287d47fae6a678ef1ab54ff62eec9e156
Mr-Hearted <hanzbausa123@gmail.com>
2023-04-28 16:28:20 +0800

    push

--- views/get-started.php
+++ views/get-started.php
@@ -10,20 +10,15 @@
 <body>
     <!-- Slide 1 -->
 <div class="tutorial-slide">
-  <h2>Welcome to our site!</h2>
-  <p>We're excited to have you here. This tutorial will help you get started.</p>
+  <h2>Welcome!!!</h2>
+  <p>You can now be able to find your desirable home in an affordable price.</p>
   <button class="next-slide-btn">Next</button>
 </div>
 
 <!-- Slide 2 -->
 <div class="tutorial-slide">
-  <h2>Getting Started</h2>
-  <p>Here are a few things you can do to get started:</p>
-  <ul>
-    <li>Update your profile information</li>
-    <li>Explore our site and find interesting content</li>
-    <li>Connect with other users and start building your network</li>
-  </ul>
+  <h2>Welcome!!!</h2>
+  <p>We will be happy to help you in promoting your business.</p>
   <form method="post" action="homepage.php">
   <input type="submit" value="Go to Other File">
 </form>
